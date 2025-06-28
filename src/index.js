2daed892b386ab58618342cb980afbab9c0c78e2
lahiru imesh <imeshbl.22@uom.lk>
2025-06-28 10:18:57 +0530

    Change index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga4';
+
+// Initialize Google Analytics
+ReactGA.initialize("G-00WQNW168X"); // Replace with your real GA4 ID
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  //<React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  //</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional: Send metrics
 reportWebVitals();
